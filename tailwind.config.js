11e272272d1792862066657ff4ed43cd9613a40b
Chikezie Joachim <56943504+chuddyjoachim@users.noreply.github.com>
2022-01-28 12:29:52 +0100

    Revert "add spotify-web-api-node, lodash and tailwind-scroll-bar"
    This reverts commit 4ff44619e63a5202e971e494e6c2469ac89fc2e9.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require('tailwind-scrollbar-hide')],
+  plugins: [],
 }
