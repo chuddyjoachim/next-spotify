3b38172d9a3bfcc21a94b070f4514a80b28540f7
Chikezie Joachim <56943504+chuddyjoachim@users.noreply.github.com>
2022-01-29 17:01:39 +0100

    add recoil atom

---
+++ src/lib/atom/playlistAtom.ts
@@ -0,0 +1,12 @@
+import { atom } from "recoil";
+
+export const playlistState = atom({
+    key: "playlistState",
+    default: null
+})
+
+export const playlistIdState = atom({
+    key: "playlistIdState",
+    default: "1zDUoFdFpVVKrshS9bJ9X7"
+    // default: "4LnTQT9pZuyXG96WS9RNzU"
+})
