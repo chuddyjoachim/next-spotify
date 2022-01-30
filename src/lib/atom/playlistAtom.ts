b6ae0039c9691d2b248ad00ea3b4d13e83c420e7
Chikezie Joachim <56943504+chuddyjoachim@users.noreply.github.com>
2022-01-30 23:16:52 +0100

    Add recoil song atom and modified playlist aton default.

--- src/lib/atom/playlistAtom.ts
+++ src/lib/atom/playlistAtom.ts
@@ -7,6 +7,5 @@ export const playlistState = atom({
 
 export const playlistIdState = atom({
     key: "playlistIdState",
-    default: "1zDUoFdFpVVKrshS9bJ9X7"
-    // default: "4LnTQT9pZuyXG96WS9RNzU"
+    default: "4LnTQT9pZuyXG96WS9RNzU"
 })
